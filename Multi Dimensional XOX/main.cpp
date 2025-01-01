e532ca0a24ffededbd2b1a42868bbc87cfcc7be0
Erhan Türker <erantrk0122@gmail.com>
2025-01-01 19:26:04 +0300

    Initial Commit

---
+++ Multi Dimensional XOX/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Multi Dimensional XOX
+//
+//  Created by Erhan Türker on 1.01.2025.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
