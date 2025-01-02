cb9ba5830c85146982eba2fe52b6e198bcbc31cb
ErhanTurker01 <erantrk0122@gmail.com>
2025-01-02 13:24:51 +0300

    Added Game class and constructor

--- Multi Dimensional XOX/Game/Game.hpp
+++ Multi Dimensional XOX/Game/Game.hpp
@@ -1,13 +1,57 @@
-//
-//  Game.hpp
-//  Multi Dimensional XOX
-//
-//  Created by Erhan Türker on 1.01.2025.
-//
+#pragma once
+#include <array>
+#include <vector>
+#include <stdexcept>
+#include <format>
+#include <iostream>
 
-#ifndef Game_hpp
-#define Game_hpp
+using u32 = uint32_t;
+using u8 = uint8_t;
+using std::array;
+using std::vector;
 
-#include <stdio.h>
+namespace XOX {
 
-#endif /* Game_hpp */
+enum class State { X, O, N, D }; /* X, O, None, Draw */
+
+template<u32 N>
+class Game {
+    friend Game<N+1>;
+protected:
+    array<array<Game<N-1>, 3>, 3> board;
+    Game<N+1>* biggerGame = nullptr;
+    State state = State::N;
+    
+public:
+    /* Constructor */
+    Game(){
+        for(auto& row : board){
+            for(Game<N-1>& game : row){
+                game.biggerGame = this;
+            }
+        }
+    }
+    
+};
+template<>
+class Game<1> {
+    friend Game<2>;
+protected:
+    array<array<State, 3>, 3> board;
+    Game<2>* biggerGame = nullptr;
+    State state = State::N;
+
+public:
+    /* Constructor */
+    Game() {
+        for (auto& row : board) {
+            row.fill(State::N);
+        }
+    }
+    
+};
+
+/* State print */
+std::ostream& operator<<(std::ostream& os, State state);
+
+}
