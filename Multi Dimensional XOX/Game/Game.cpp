0965a9ccf8a0a35e7e29f6c66931a9f1bb6669c9
ErhanTurker01 <erantrk0122@gmail.com>
2025-01-02 13:26:16 +0300

    Added cout fun for States

--- Multi Dimensional XOX/Game/Game.cpp
+++ Multi Dimensional XOX/Game/Game.cpp
@@ -1,8 +1,17 @@
-//
-//  Game.cpp
-//  Multi Dimensional XOX
-//
-//  Created by Erhan Türker on 1.01.2025.
-//
-
 #include "Game.hpp"
+#include <iostream>
+
+namespace XOX {
+
+std::ostream& operator<<(std::ostream& os, State state) {
+    switch (state) {
+        case State::X: os << "X"; break;
+        case State::O: os << "O"; break;
+        case State::N: os << "None"; break;
+        case State::D: os << "Draw"; break;
+        default: os << "Unknown"; break;
+    }
+    return os;
+}
+
+} // namespace XOX
