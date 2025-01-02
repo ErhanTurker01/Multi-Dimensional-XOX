6f1b7b3861e686cba3bb6c77f0fc4fe2b4483412
ErhanTurker01 <erantrk0122@gmail.com>
2025-01-02 13:21:15 +0300

    Created Game files

---
+++ Multi Dimensional XOX/Game/Game.cpp
@@ -0,0 +1,8 @@
+//
+//  Game.cpp
+//  Multi Dimensional XOX
+//
+//  Created by Erhan Türker on 1.01.2025.
+//
+
+#include "Game.hpp"
---
+++ Multi Dimensional XOX/Game/Game.hpp
@@ -0,0 +1,13 @@
+//
+//  Game.hpp
+//  Multi Dimensional XOX
+//
+//  Created by Erhan Türker on 1.01.2025.
+//
+
+#ifndef Game_hpp
+#define Game_hpp
+
+#include <stdio.h>
+
+#endif /* Game_hpp */
